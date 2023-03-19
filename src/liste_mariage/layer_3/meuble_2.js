c5334eb5283d8c4c2dcdd272f1762ed8217edc82
MarineBenjamin <69369042+MarineBenjamin@users.noreply.github.com>
2023-03-19 14:49:53 +0100

    Update meuble_2.js
    Fixing meuble2.PNG ext

--- src/liste_mariage/layer_3/meuble_2.js
+++ src/liste_mariage/layer_3/meuble_2.js
@@ -11,7 +11,7 @@ meuble2.set_parent(layer3);
 
 let meuble2Img = new Element(IMG);
 meuble2Img.set_id("meuble2Img");
-meuble2Img.set_src(rpath + "../../assets/meuble2.png");
+meuble2Img.set_src(rpath + "../../assets/meuble2.PNG");
 meuble2Img.set_width("75%");
 meuble2Img.set_height("90%");
 meuble2Img.set_position("absolute");
