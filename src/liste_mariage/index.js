279d55e9adedb071a9bb3350d92fa7327159e532
MarineBenjamin <69369042+MarineBenjamin@users.noreply.github.com>
2023-03-19 14:06:39 +0100

    Add files via upload

---
+++ src/liste_mariage/index.js
@@ -0,0 +1,65 @@
+let previousX = document.body.clientWidth;
+let previousY = document.body.clientHeight;
+
+
+function window_resize_event_handler()
+{
+    let x = window.innerWidth;
+    let y = window.innerHeight;
+
+	if(x < 450 && y < 950) // Smartphone
+	{
+		
+	}
+	else
+	{
+		
+		if(x < 1280) 
+		{ 
+			layer1.set_width("1280px");
+			layer2.set_width("1280px");
+			layer3.set_width("1280px");
+
+			document.body.style.overflowX = "scroll";
+
+		}
+		else
+		{
+			layer1.set_width(x + "px");
+			layer2.set_width(x + "px");
+			layer3.set_width(x + "px");
+			document.body.style.overflowX = "hidden";
+		}
+
+
+
+		/*if(y < 720)
+		{
+			layer1.set_top("72px");
+		}
+		else
+		{
+			layer1.set_top(0.1*y + "px");		
+		}*/
+
+		layer1.set_height(x*0.375 + "px");
+
+		
+
+		layer2.set_top(Math.max(parseInt(layer1.top(), 10) + parseInt(layer1.height(), 10), 72) + 0.04*y + "px");
+		layer2.set_height(Math.max(y - parseInt(layer2.top()), 144) + "px");
+
+		layer3.set_top(Math.max(parseInt(layer2.top()) + parseInt(layer2.height()), y) + "px");
+
+		let style = getComputedStyle(noce.element());
+		layer4.set_top( parseInt(layer3.top()) + parseInt(style.top,10) + parseInt(style.height,10) + 100 + "px");
+		console.log(layer4.top())
+
+	}
+
+
+}
+
+
+window.addEventListener("resize", window_resize_event_handler);
+window_resize_event_handler();
