bc511ac7c7896baa7e5bda6213f7cf5b09e5c0ac
MarineBenjamin <69369042+MarineBenjamin@users.noreply.github.com>
2023-03-22 18:20:44 +0100

    Update reception.js

--- src/components/layer_3/reception.js
+++ src/components/layer_3/reception.js
@@ -36,7 +36,7 @@ layer3ReceptionTextTitle.set_width("100%");
 
 layer3ReceptionTextCore.set_innerHTML("<p>La r&eacuteception aura lieu apr&egraves la C&eacuter&eacutemonie &agrave la Ferme Sainte Anne de Langres.<br/><br/><br/> \
 Adresse : 380 chemin de la ferme Sainte Anne 52200 Langres.<br/><br/><br/> \
-Coordonn&eacutees GPS : 47.860365285669985, 5.313605032819924</p>");
+Coordonn&eacutees GPS : 47.860365285669985, 5.313605032819924.</p>");
 layer3ReceptionTextCore.set_fontFamily("Supermercado One");
 layer3ReceptionTextCore.set_fontWeight("bold");
 layer3ReceptionText.set_display("flex");
